391f47ced0d2628812da0b36c7a57cba70c6d2b4
cirosanchez <ciroandressanchezbuitrago@gmail.com>
2024-10-07 11:02:39 -0500

    fixed build script part 4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.plugin.kotlinToolingVersion
-
 plugins {
     kotlin("jvm") version "2.0.20"
     id("com.github.johnrengelman.shadow") version "8.1.1"
@@ -9,8 +7,6 @@ plugins {
 group = "me.cirosanchez"
 version = "1.0-SNAPSHOT"
 
-val sourceCompatibility = 17
-val targetCompatibility = 17
 
 repositories {
     mavenCentral()
@@ -33,14 +29,6 @@ dependencies {
 val targetJavaVersion = 17
 kotlin {
     jvmToolchain(targetJavaVersion)
-}
-
-val javaVersion = 21
-val javaVersionEnumMember = JavaVersion.valueOf("VERSION_$javaVersion")
-
-java {
-    sourceCompatibility = javaVersionEnumMember
-    targetCompatibility = javaVersionEnumMember
 }
 
 tasks.build {
