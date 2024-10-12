789f3d8970b1f3a2e28ba10c714c97be13d48643
cirosanchez <ciroandressanchezbuitrago@gmail.com>
2024-10-12 13:04:01 -0500

    Debugging #6

--- src/main/kotlin/me/cirosanchez/clib/extension/CommandSender.kt
+++ src/main/kotlin/me/cirosanchez/clib/extension/CommandSender.kt
@@ -11,6 +11,8 @@ fun CommandSender.send(message: String){
 }
 
 fun CommandSender.send(path: String, vararg placeholder: Placeholder){
-    val string = CLib.get().messagesFile.getString(path) ?: "NULL"
+    val messages = CLib.get().messagesFile ?: throw IllegalStateException("You didn't enable messages while setting up cLib!")
+
+    val string = messages.getString(path) ?: "NULL"
     CLib.get().audiences.sender(this).sendMessage(string.placeholders(*placeholder).colorize())
 }
