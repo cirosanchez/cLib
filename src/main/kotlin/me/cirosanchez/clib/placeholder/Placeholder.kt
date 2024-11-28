2758911016f5d6604f0ac5954b34b2dbc1f3b714
cirosanchez <ciroandressanchezbuitrago@gmail.com>
2024-11-28 12:12:56 -0500

    Added a method to Placeholder.kt
    Took 6 minutes

--- src/main/kotlin/me/cirosanchez/clib/placeholder/Placeholder.kt
+++ src/main/kotlin/me/cirosanchez/clib/placeholder/Placeholder.kt
@@ -1,3 +1,7 @@
 package me.cirosanchez.clib.placeholder
 
-data class Placeholder(val target: String, val replacement: String)
+data class Placeholder(val target: String, val replacement: String){
+    fun replace(string: String): String {
+        return string.replace(target, replacement)
+    }
+}
