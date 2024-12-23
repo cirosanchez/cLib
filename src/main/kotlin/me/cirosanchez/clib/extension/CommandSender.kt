c9d439df34a64a9640930b05b464210f448e03d3
cirosanchez <ciroandressanchezbuitrago@gmail.com>
2024-12-22 22:24:48 -0500

    added new method yay
    Took 5 minutes

--- src/main/kotlin/me/cirosanchez/clib/extension/CommandSender.kt
+++ src/main/kotlin/me/cirosanchez/clib/extension/CommandSender.kt
@@ -5,6 +5,7 @@ import me.cirosanchez.clib.logger
 import me.cirosanchez.clib.placeholder.Placeholder
 import org.bukkit.command.CommandSender
 import java.util.logging.Logger
+import kotlin.text.Typography.section
 
 fun CommandSender.send(message: String){
     CLib.get().audiences.sender(this).sendMessage(message.colorize())
@@ -18,4 +19,21 @@ fun CommandSender.sendColorizedMessageFromMessagesFile(path: String, vararg plac
 
     val string = CLib.get().messagesFile.getString(path) ?: "NULL"
     CLib.get().audiences.sender(this).sendMessage(string.placeholders(*placeholder).colorize())
+}
+
+fun CommandSender.sendColorizedMessageFromMessagesFileList(path: String, vararg placeholder: Placeholder){
+    if(!CLib.get().messages) {
+        logger().warning("You didn't setup messages in cLib main function! In order to use integrated messages.yml use it.")
+        return
+    }
+
+    val section = CLib.get().messagesFile.getConfigurationSection(path)!!
+
+    val list = section.getKeys(false)
+
+    list.forEach {
+        val string = section.getString("$it")
+
+        this.sendColorizedMessageFromMessagesFile("$path.$it", *placeholder)
+    }
 }
